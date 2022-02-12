d81d76435c15c99cd750e2ce0320d65f52a3ab04
Darwish Mat Zain <darwish@darwishzain.com>
2022-02-12 12:04:41 -0800

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,12 @@
+import Tkinter as tk
+
+root = tk.Tk()
+root.title('Link')
+
+
+frameMain = tk.Frame(root, height=300, width=500)
+frameMain.grid(row=0, column=0)
+#btnOr = tk.Button(root, image='../img/or.ump.png')
+#btnOr.grid(row=0, column=0)
+
+root.mainloop()
