39e8e8875eb9cf7d7b84220c65c7f2e3defd8484
Darwish Mat Zain <darwish@darwishzain.com>
2022-02-19 10:01:21 -0800

    Update 20022022 0200
    Divide the interface into section

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# ump-link
- 
+# UMP-link
+##### UMP-linkv0.1.5
+![v0.1.5](img/ump-link-v0.1.5.png)
+### Introduction
+You are a studying at University of Malaysia Pahang (UMP) and you need to access certain UMP's website for an urgent matter. What would you do ?
+
+By using our solution, you can access the website with a click of a button and it will automatically open a browser to access the website.
+
+### Intended Use
+1. User setup ```UMP-link``` as a startup application.
+2. ```UMP-link``` run on startup.
+3. User click on desired website.
+
+### Getting Started
+#### Installation (Windows)
+##### ZIP
+1. Download application ```.zip``` file from [here](https://github.com/darwishzain/ump-link/releases/download/neo-release/UMP-Link.v0.1.5.zip).
+2. Extract ```.zip``` file.
+3. Open folder ```./UMP-link.vx.x.x/ump-link```.
+4. Open file ```ump-link.exe```.
+
+### Devlog
+* Version 0.1.5
+  - added link for UMP KALAM website (https://kalam.ump.edu.my)
+  - added link for UMP ECommunity website (https://community.ump.edu.my)
+  - added link for UMP Open Registration website (https://or.ump.edu.my)
+  - added link for UMP Digital Assessment System website (https://udas.ump.edu.my)
+
+![v0.1.5](img/ump-link-v0.1.5.png)
+
+* Version 0.1.6 (development)
+  - added link for UMP EFind website (https://efind.ump.edu.my)
+  - added link for donation (https://ko-fi/darwishzain)
+
+![v0.1.5](img/ump-link-v0.1.6-dev.png)
--- app/ump-link.py
+++ app/ump-link.py
@@ -1,25 +1,36 @@
-import Tkinter as tk
+import tkinter as tk
+#! linux : Tkinter
+#! win10 : tkinter
 import os,sys
 import csv
+import time
+
+developer = 'Darwish Zain'
+version = '0.1.6 - dev'
 
 root = tk.Tk()
-root.title('UMP-link on '+os.name)
+root.title('UMP-link on '+ sys.platform + ' v' +version)
+
 if os.name=='posix':
     file = './data/command-linux.csv'#!linux
 elif os.name=='nt':
     file = './data/command-win10.csv'#!windows10
 
-print('Running ump-link on '+os.name)
-
+print('Running ump-link on '+ sys.platform)
+time.sleep(1)
+print('Developed by '+ developer)
+time.sleep(1)
+print('Version '+ version)
+time.sleep(1)
 #? csv format,id, command, name, row, column
-linecsv = []#* array variable for data from csv file
+line = []#* array variable for data from csv file
 with open(file) as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
-        linecsv.append(row)
-    # array : linecsv
+        line.append(row)
+    # array : line
 
-#print(linecsv)#* checking if data from csv file came through
+#print(line)#* checking if data from csv file came through
 #? run cli command
 def command(cmd):
     os.system(cmd)
@@ -27,38 +38,45 @@ def command(cmd):
 #? frame where the button is in
 fmain = tk.Frame(root, bg='#F0F0F0', height=300, width=500)
 fmain.grid(row=0, column=0)
+titlebar = tk.Frame(fmain, bg='#FFF')
+titlebar.grid(row=0, column=0, columnspan=5)
+content = tk.Frame(fmain)
+content.grid(row=1, column=0)
+devbar = tk.Frame(fmain, bg='#FFF')
+devbar.grid(row=2, column=0, columnspan=5)
 
 def umplink():
-    kalamBtn = tk.Button(fmain, bg='#F0F0F0', height=5, width=5,text=linecsv[0][2],command=lambda:command(linecsv[0][1]), relief='flat')
-    kalamBtn.grid(row=linecsv[0][3], column=linecsv[0][4])
-    ecommBtn = tk.Button(fmain, height=5, width=5,text=linecsv[1][2],command=lambda:command(linecsv[1][1]), relief='flat')
-    ecommBtn.grid(row=linecsv[1][3], column=linecsv[1][4])
-    orBtn = tk.Button(fmain, height=5, width=5,text=linecsv[2][2],command=lambda:command(linecsv[2][1]), relief='flat')
-    orBtn.grid(row=linecsv[2][3], column=linecsv[2][4])
-    udasBtn = tk.Button(fmain, height=5, width=5,text=linecsv[3][2],command=lambda:command(linecsv[3][1]), relief='flat')
-    udasBtn.grid(row=linecsv[3][3], column=linecsv[3][4])
-    efindBtn = tk.Button(fmain, height=5, width=5,text=linecsv[4][2],command=lambda:command(linecsv[4][1]), relief='flat')
-    efindBtn.grid(row=linecsv[4][3], column=linecsv[4][4])
+    kalamBtn = tk.Button(content, bg='#F0F0F0', height=5, width=10,text=line[0][2],command=lambda:command(line[0][1]), relief='flat')
+    kalamBtn.grid(row=line[0][3], column=line[0][4])
+    ecommBtn = tk.Button(content, height=5, width=10,text=line[1][2],command=lambda:command(line[1][1]), relief='flat')
+    ecommBtn.grid(row=line[1][3], column=line[1][4])
+    orBtn = tk.Button(content, height=5, width=10,text=line[2][2],command=lambda:command(line[2][1]), relief='flat')
+    orBtn.grid(row=line[2][3], column=line[2][4])
+    udasBtn = tk.Button(content, height=5, width=10,text=line[3][2],command=lambda:command(line[3][1]), relief='flat')
+    udasBtn.grid(row=line[3][3], column=line[3][4])
+    efindBtn = tk.Button(content, height=5, width=10,text=line[4][2],command=lambda:command(line[4][1]), relief='flat')
+    efindBtn.grid(row=line[4][3], column=line[4][4])
 
 umplink()
 #? button to link Darwish Zain's website
-devsite = tk.Button(fmain, text=linecsv[5][2], command=lambda:command(linecsv[5][1]), relief='flat')
-devsite.grid(row=linecsv[5][3], column=linecsv[5][4], columnspan=3, sticky='nesw')
+devsite = tk.Button(devbar, text=line[5][2], command=lambda:command(line[5][1]), relief='flat')
+devsite.grid(row=line[5][3], column=line[5][4], columnspan=3, sticky='nesw')
 
 #? button to link Darwish Zain's github
-github = tk.Button(fmain, text=linecsv[6][2], command=lambda:command(linecsv[6][1]), relief='flat')
-github.grid(row=linecsv[6][3], column=linecsv[6][4], columnspan=1, sticky='nesw')
+github = tk.Button(devbar, text=line[6][2], command=lambda:command(line[6][1]), relief='flat')
+github.grid(row=line[6][3], column=line[6][4], columnspan=1, sticky='nesw')
 
-donate = tk.Button(fmain, bg='#00AC9F', text=linecsv[7][2], command=lambda:command(linecsv[7][1]), relief='flat')
-donate.grid(row=linecsv[7][3], column=linecsv[7][4], columnspan=1, sticky='nesw')
+donate = tk.Button(devbar, bg='#00AC9F', fg='#FFFFFF', text=line[7][2], command=lambda:command(line[7][1]), relief='flat')
+donate.grid(row=line[7][3], column=line[7][4], columnspan=1, sticky='nesw')
 
+root.eval('tk::PlaceWindow . center')#? Application apppear at center of screen
 root.mainloop()
 
 #! unused code
 #? just for my reference later on
 #btnkalam = tk.Button(fmain, text=d[1][2],command=lambda:command(d[1][1]))
 #btnkalam.grid(row=1, column=0)
-#d[1][0] = tk.Button(fmain, height=5, width=5,text=d[1][2],command=lambda:command(d[1][1]))
+#d[1][0] = tk.Button(fmain, height=5, width=10,text=d[1][2],command=lambda:command(d[1][1]))
 #d[1][0].grid(row=1, column=1)
 #btnOr = tk.Button(root, image='../img/or.ump.png')
 #btnOr.grid(row=0, column=0)
